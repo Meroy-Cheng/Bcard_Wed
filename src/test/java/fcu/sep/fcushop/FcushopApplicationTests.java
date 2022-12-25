65af5a4e76b56ec9dba0f4f0cb661b42e0c895d0
macaronYa <90053258+macaronYa@users.noreply.github.com>
2022-12-25 20:11:50 +0800

    Update FcushopApplicationTests.java

--- src/test/java/fcu/sep/fcushop/FcushopApplicationTests.java
+++ src/test/java/fcu/sep/fcushop/FcushopApplicationTests.java
@@ -1,13 +1,32 @@
 package fcu.sep.fcushop;
 
+import fcu.sep.fcushop.model.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FcushopApplicationTests {
 
+	Index sc = new Index();
 	@Test
-	void contextLoads() {
+	void testPWLength() {
+		Assertions.assertEquals("Error",sc.index("123","12345"));
+		Assertions.assertEquals("Success",sc.index("123","123456"));
+	}
+
+	@Test
+	void testSamePW(){
+		Assertions.assertEquals("請再次輸入密碼",sc.index2("123456",""));
+		Assertions.assertEquals("密碼不一致",sc.index2("123456","234566"));
+		Assertions.assertEquals("Success",sc.index2("123456","123456"));
+	}
+
+	@Test
+	void testSpace(){
+		Assertions.assertEquals("Error",sc.index("123456",""));
+		Assertions.assertEquals("Error",sc.index("","123456"));
+		Assertions.assertEquals("Success",sc.index("123456","123456"));
 	}
 
 }
